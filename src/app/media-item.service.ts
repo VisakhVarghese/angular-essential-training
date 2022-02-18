868f88244182feb96097c39269846d303ae5a2b3
Visakh Varghese <84069051+VisakhVarghese@users.noreply.github.com>
2022-02-18 11:28:33 +0530

    first

---
+++ src/app/media-item.service.ts
@@ -0,0 +1,96 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class MediaItemService {
+  mediaItems = [
+    {
+      id: 1,
+      name: "Firebug",
+      medium: "Series",
+      category: "Science Fiction",
+      year: 2010,
+      watchedOn: 1294166565384,
+      isFavorite: false,
+    },
+    {
+      id: 2,
+      name: "The Small Tall",
+      medium: "Movies",
+      category: "Comedy",
+      year: 2015,
+      watchedOn: null,
+      isFavorite: true,
+    },
+    {
+      id: 3,
+      name: "The Redemption",
+      medium: "Movies",
+      category: "Action",
+      year: 2016,
+      watchedOn: null,
+      isFavorite: false,
+    },
+    {
+      id: 4,
+      name: "Hoopers",
+      medium: "Series",
+      category: "Drama",
+      year: null,
+      watchedOn: null,
+      isFavorite: true,
+    },
+    {
+      id: 5,
+      name: "Happy Joe: Cheery Road",
+      medium: "Movies",
+      category: "Action",
+      year: 2015,
+      watchedOn: 1457166565384,
+      isFavorite: false,
+    },
+  ];
+
+  constructor(private http: HttpClient) {}
+  get(medium: string) {
+    // return this.mediaItems;
+    const getOptions = {
+      params: {
+        medium,
+      },
+    };
+    return this.http.get<MediaItemResponse>("mediaItems", getOptions).pipe(
+      map((res) => {
+        return res.mediaItems;
+      })
+    );
+  }
+
+  add(mediaitem) {
+    this.mediaItems.push(mediaitem);
+  }
+
+  delete(mediaItem) {
+    const index = this.mediaItems.indexOf(mediaItem);
+    if (index >= 0) {
+      this.mediaItems.splice(index, 1);
+    }
+  }
+}
+
+export interface MediaItem {
+  id: number;
+  name: string;
+  medium: string;
+  category: string;
+  year: number;
+  watchedOn: number;
+  isFavorite: boolean;
+}
+
+interface MediaItemResponse {
+  mediaItems: MediaItem[];
+}
